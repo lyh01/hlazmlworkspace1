7fdf881eb25e6192739050588ba1d024c1531d64
Huy <huy_ly@hotmail.com>
2021-02-04 23:03:24 +0000

    adding new files

---
+++ train.py
@@ -0,0 +1,10 @@
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+def main():
+   with open("01.run-experiment.ipynb","r") as fp:
+      nb = nbformat.read(fp, as_version=4)
+   ep = ExecutePreprocessor(timeout=600, kernel_name="python")
+   ep.preprocess(nb, {"metadata": {"path":"./"}})
+if __name__ == "__main__":
+   main()
