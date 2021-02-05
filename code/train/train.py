7001b4b506f5924dadb6eb04158c50fb556eaa62
Huy <huy_ly@hotmail.com>
2021-02-05 20:24:13 +0000

    add jupyter

--- code/train/train.py
+++ code/train/train.py
@@ -23,7 +23,7 @@ def main():
 
    with open("01.run-experiment.ipynb","r") as fp:
       nb = nbformat.read(fp, as_version=4)
-   ep = ExecutePreprocessor(timeout=600, kernel_name="python3"
+   ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
    ep.preprocess(nb, {"metadata": {"path":"./"}})
 
 if __name__ == "__main__":
