57bc48dc26f5350aaccaa58cbc99a0264c3812e6
Huy <huy_ly@hotmail.com>
2021-02-05 16:09:28 +0000

    kernel = Python 3.7 - AzureML

--- code/train/train.py
+++ code/train/train.py
@@ -4,7 +4,7 @@ from nbconvert.preprocessors import ExecutePreprocessor
 def main():
    with open("01.run-experiment.ipynb","r") as fp:
       nb = nbformat.read(fp, as_version=4)
-   ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+   ep = ExecutePreprocessor(timeout=600, kernel_name="Python 3.7 - AzureML")
    ep.preprocess(nb, {"metadata": {"path":"notebooks/"}})
 if __name__ == "__main__":
    main()
