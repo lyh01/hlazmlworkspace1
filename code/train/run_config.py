250a1b3bb79a5ec87df4b3dc84ac82f1649ee708
Huy <huy_ly@hotmail.com>
2021-02-05 04:49:44 +0000

    added run_config.py

---
+++ code/train/run_config.py
@@ -0,0 +1,15 @@
+import logging
+from azureml.core import Workspace, Experiment, ScriptRunConfig
+def main(workspace):
+    """
+      input: workspace
+      output: ScriptRunConfig
+      
+      For more information see: https://github.com/Azure/aml-run 
+
+    """
+    logging.info(f"run_config")
+    script_run_config = ScriptRunConfig(
+         source_directory=".", script="train.py", compute_target="amlcompute3"
+         )
+    return script_run_config
