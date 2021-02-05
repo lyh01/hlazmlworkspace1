ce10eeee15e66fad2bde744f95f691d33d90fb09
Huy <huy_ly@hotmail.com>
2021-02-05 15:22:55 +0000

    kernel = python

--- code/train/train.py
+++ code/train/train.py
@@ -4,7 +4,7 @@ from nbconvert.preprocessors import ExecutePreprocessor
 def main():
    with open("01.run-experiment.ipynb","r") as fp:
       nb = nbformat.read(fp, as_version=4)
-   ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+   ep = ExecutePreprocessor(timeout=600, kernel_name="python")
    ep.preprocess(nb, {"metadata": {"path":"notebooks/"}})
 if __name__ == "__main__":
    main()
